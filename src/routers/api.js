79e58bcb2d57c20a6331aab14b0a96dfc881827d
itminhchau <daominhchau18@gmail.com>
2023-07-21 11:55:38 +0700

    [Add] comment

--- src/routers/api.js
+++ src/routers/api.js
@@ -8,7 +8,9 @@ const initAPIRouter = (app) => {
 
     router.get('/api/v1/group', groupController.getGroup)
     router.get('/api/v1/user', userController.getUser)
+    // truyền theo query param (?groupId=2)
     router.get('/api/v1/user-group', userController.getUseOfGroup)
+    // truyền theo body : là cục data ({name: "minh",age:20})
     router.put('/api/v1/update/user', userController.updateUser)
 
     return app.use('/', router)
